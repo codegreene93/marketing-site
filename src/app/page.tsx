acefcdfaf0bd9855b6ef7926f0588df2cd298bc0
Amy Greene <amy.greene@immersivelabs.com>
2025-12-12 09:37:45 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import homeImg from "public/home.jpg"
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-  <div>Home Page</div>
-  );
+    <Hero title="Professional Cloud Hosting" imageAlt={"Car manufacturer"} imageData={homeImg} />
+  )
 }
