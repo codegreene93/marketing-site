4ce7fb32cc0db12d1b9a728169c0756ed153addc
Amy Greene <amy.greene@immersivelabs.com>
2026-01-14 08:27:48 +0000

    fix typo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return(
         <div className={"w-full absolute text-white z-10"}>
             <nav className={"container relative flex flex-wrap items-center justify-between mx-auto p-8"}>
-            <Link href={"/"} classname={"font-bold text-3xl"}>Home</Link>
+            <Link href={"/"} className={"font-bold text-3xl"}>Home</Link>
                 <div className={"space-x-4 text-3xl"}>
             <Link href={"/performance"}>Performance</Link>
             <Link href={"/reliability"}>Reliability</Link>
